91edc5473d6dbc5a1afcf05b78c9e9d850ad2b9d
shruthi-sai <89636224+shruthi-sai@users.noreply.github.com>
2021-08-31 19:54:50 +0530

    DECLARING_VARIABLE_OF_ALL_DATA_TYPES.java

---
+++ DECLARING_VARIABLE_OF_ALL_DATA_TYPES.java
@@ -0,0 +1,15 @@
+//Declaring Variables of all Data Types
+public class DECLARING_VARIABLE_OF_ALL_DATA_TYPES {
+
+	public static void main(String[] args) {
+     
+		int number = 26;
+		float floatnumber = 26.99f;
+		char character = 'S';
+		boolean booleanvalue = true;
+		String name = "SaiShruthi";
+		
+		
+	}
+
+}
