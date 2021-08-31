7f4977c53250b097a39b96e30d14beeb0fe08a56
shruthi-sai <89636224+shruthi-sai@users.noreply.github.com>
2021-08-31 19:55:45 +0530

    JAVA_IS_INSTALLED_ON_PC.java

---
+++ JAVA_IS_INSTALLED_ON_PC.java
@@ -0,0 +1,19 @@
+//Check whether Java is installed on PC
+public class JAVA_IS_INSTALLED_ON_PC {
+
+	public static void main(String[] args) {
+  
+		System.out.println("\nJava Version: " + System.getProperty("java.version"));
+		System.out.println("\nJava Runtime Version: " + System.getProperty("java.runtime.version"));
+		System.out.println("\nJava Home: " + System.getProperty("java.home"));
+		System.out.println("\nJava Vendor: " + System.getProperty("java.vendor"));
+		System.out.println("\nJava Vendor URL: " + System.getProperty("java.vendor.url"));
+		System.out.println("\nJava Class Path: " + System.getProperty("java.class.path") + "\n");
+
+
+
+
+
+	}
+
+}
