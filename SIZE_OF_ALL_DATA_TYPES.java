fe2143b1a3789ed710f805f9b820fc47b028bd50
shruthi-sai <89636224+shruthi-sai@users.noreply.github.com>
2021-08-31 19:55:24 +0530

    SIZE_OF_ALL_DATA_TYPES.java

---
+++ SIZE_OF_ALL_DATA_TYPES.java
@@ -0,0 +1,14 @@
+//Find Size of all Data Types
+public class SIZE_OF_ALL_DATA_TYPES {
+
+	public static void main(String[] args) {
+
+		System.out.println("Size of int: " + (Integer.SIZE/8) + "bytes");
+		System.out.println("Size of long: " + (Long.SIZE/8) + "bytes");
+		System.out.println("Size of char: " + (Character.SIZE/8) + "bytes");
+		System.out.println("Size of float: " + (Float.SIZE/8) + "bytes");
+		System.out.println("Size of double: " + (Double.SIZE/8) + "bytes");
+       
+	}
+
+}
